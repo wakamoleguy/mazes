7fed8763890a4328beccaeea41e655c56f5a8dbb
Will Mitchell <wakamoleguy@gmail.com>
2017-08-30 18:47:45 -0700

    A working static homepage

--- clean/web/index.js
+++ clean/web/index.js
@@ -3,20 +3,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 // Drivers
-const authDriver = require('../adapters/auth/passwordless');
+// TODO - const authDriver = require('../adapters/auth/passwordless');
 
-const auth = require('./auth')(authDriver);
-const api = require('./api');
-const app = require('./app');
+//const auth = require('./auth')(authDriver);
+//const api = require('./api');
+//const app = require('./app');
 
 const combinedApp = express();
 combinedApp.use(bodyParser.json());
-combinedApp.use(bodyParser.urlencoded());
+combinedApp.use(bodyParser.urlencoded({ extended: false }));
 
 combinedApp.use(express.static(__dirname + '/static'));
-combinedApp.use(auth);
-combinedApp.use('/api', api);
-combinedApp.use('/app', app);
+//combinedApp.use(auth);
+//combinedApp.use('/api', api);
+//combinedApp.use('/app', app);
 
 const server = http.createServer(combinedApp);
 server.listen(process.env.PORT || 3000);
