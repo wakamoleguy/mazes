b99d2de9d1cd5b3eb7fcbc29ba4485646e69570a
Will Mitchell <wakamoleguy@gmail.com>
2017-09-23 19:23:05 -0700

    Some unimplemented (read: failing) unit tests for MVC

---
+++ clean/web/app/controllers/maze.test.js
@@ -0,0 +1,48 @@
+describe('Maze controller', () => {
+
+    describe('browse', () => {
+
+        it('should fetch the current user details', (done) => {
+
+            done.fail('Unimplemented');
+        });
+
+        it('should fetch the current user\'s mazes', (done) => {
+            done.fail('Unimplemented');
+        });
+
+        it('should render the maze list index page', (done) => {
+            done.fail('Unimplemented');
+        });
+    });
+
+    describe('read', () => {
+
+        it('should fetch the current user details', (done) => {
+
+            done.fail('Unimplemented');
+        });
+
+        it('should fetch the maze from the repository', (done) => {
+
+            done.fail('Unimplemented');
+        });
+
+        it('should render the maze details page', (done) => {
+
+            done.fail('Unimplemented');
+        });
+    });
+
+    describe('edit', () => {
+
+    });
+
+    describe('add', () => {
+
+    });
+
+    describe('delete', () => {
+
+    });
+});
---
+++ clean/web/app/controllers/revision.test.js
@@ -0,0 +1,66 @@
+describe('Revision controller', () => {
+
+    describe('browse', () => {
+    });
+
+    describe('read', () => {
+
+        it('should fetch the current user details', (done) => {
+
+            done.fail('Unimplemented');
+        });
+
+        it('should fetch the revision details', (done) => {
+
+            done.fail('Unimplemented');
+        });
+
+        it('should render the revision read page if locked', (done) => {
+
+            done.fail('Unimplemented');
+        });
+
+        it('should render the revision edit page if unlocked', (done) => {
+
+            done.fail('Unimplemented');
+        });
+    });
+
+    describe('edit', () => {
+
+        it('should save the revised maze revision', (done) => {
+
+            done.fail('Unimplemented');
+        });
+
+        it('should return success', (done) => {
+
+            done.fail('Unimplemented');
+        });
+    });
+
+    describe('add', () => {
+    });
+
+    describe('delete', () => {
+
+    });
+
+    describe('run', () => {
+
+        it('should fetch the current user details', (done) => {
+
+            done.fail('Unimplemented');
+        });
+
+        it('should fetch the current revision', (done) => {
+
+            done.fail('Unimplemented');
+        });
+
+        it('should render the maze runner page', (done) => {
+
+            done.fail('Unimplemented');
+        });
+    });
+});
