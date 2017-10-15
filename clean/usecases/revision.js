3e24f40fe0f474d07ee4e5515aeee47e378f3b52
Will Mitchell <wakamoleguy@gmail.com>
2017-10-14 17:35:19 -0700

    Implement revision read use case

--- clean/usecases/revision.js
+++ clean/usecases/revision.js
@@ -2,6 +2,7 @@ module.exports = {
 
     read(mazeRepository, revisionId) {
 
+        return mazeRepository.revision.read(revisionId);
     },
 
     edit(mazeRepository, revisionId, revision) {
--- clean/usecases/revision.test.js
+++ clean/usecases/revision.test.js
@@ -1,15 +1,46 @@
+const usecases = require('./revision');
+
 describe('Revision use cases', () => {
 
     describe('Reading a revision', () => {
 
+        let mazeRepository;
+
+        beforeEach(() => {
+
+            mazeRepository = {
+                revision: jasmine.createSpyObj('mazeRepository', [
+                    'read'
+                ])
+            };
+
+        });
+
         it('should resolve to a revision', (done) => {
 
-            done.fail('Unimplemented');
+            mazeRepository.revision.read.and.returnValue(Promise.resolve({
+                id: '123',
+                data: 'foo'
+            }));
+
+            usecases.read(mazeRepository, '123').then((revision) => {
+
+                expect(revision).toEqual({ id: '123', data: 'foo' });
+                done();
+            }).catch(done.fail);
+
         });
 
         it('should resolve to null if no revision is found', (done) => {
 
-            done.fail('Unimplemented');
+            mazeRepository.revision.read.and.returnValue(Promise.resolve(null));
+
+            usecases.read(mazeRepository, '123').then((revision) => {
+
+                expect(revision).toBe(null);
+                done();
+            }).catch(done.fail);
+
         });
     });
 
